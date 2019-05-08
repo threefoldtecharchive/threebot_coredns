8b79587c43959960c23ce51f35146171aef8cede
Ahmed T. Youssef <xmonader@gmail.com>
2019-05-08 12:23:30 +0200

    handle ip4 and ip6 in incoming addresses

--- threebot.go
+++ threebot.go
@@ -128,6 +128,9 @@ func (threebot *Threebot) get(key string) (*Record, error) {
 		ThreeBotRecord `json:"record"`
 	}
 	// whoever responds is enough
+	rec := new(Record)
+	rec.A = []A_Record{}
+	rec.AAAA = []AAAA_Record{}
 	for _, explorer := range threebot.Explorers {
 		whoisUrl := explorer+"/explorer/whois/3bot/"+key
 		resp, error := http.Get(whoisUrl)
@@ -142,20 +145,27 @@ func (threebot *Threebot) get(key string) (*Record, error) {
 				continue
 			}
 			// TODO: handle multiple records and agree on standard return of IPv4 for locations where 3bots are running on.
-			rec := new(Record)
 			for _, addr := range(whoisResp.Addresses) {
-				// return on the first valid address.
+
 				theIp := net.ParseIP(addr)
-				if theIp == nil {
-					return nil, fmt.Errorf("couldn't parse address ", addr, "  of record for 3bot with key ", key)
+				if theIp != nil {
+					if  theIp.To4() != nil {
+						rec.A = append(rec.A, A_Record{Ip: theIp, Ttl: 300})
+						continue
+					}
+					if  theIp.To16() != nil {
+						rec.AAAA = append(rec.AAAA, AAAA_Record{Ip: theIp})
+						continue
+					}
 				}
-				rec.A = []A_Record{
-					{Ip: theIp, Ttl:300},
-				}
-				return rec, nil
+				// TODO: hostnames.
 			}
 		}
 	}
+	if len(rec.A) + len(rec.AAAA) > 0 {
+		return rec, nil
+	}
+
 	return nil, fmt.Errorf("couldn't get record for 3bot with key ", key)
 }
 
